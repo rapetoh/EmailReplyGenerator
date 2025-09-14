86af4ff15f06f868d3014244c3c852451c7c59dc
Roch APETOH D. <rapetoh@hotmail.com>
2025-09-14 07:03:36 -0500

    Looks like there was a small issue in our src folder. Fixed it

---
+++ Email_Reply_Generator/src/main/java/com/EmailReplyGenrator/Email_Reply_Generator/EmailReplyGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.EmailReplyGenrator.Email_Reply_Generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailReplyGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailReplyGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/EmailReplyGenrator/Email_Reply_Generator/EmailReplyGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EmailReplyGenrator.Email_Reply_Generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailReplyGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
